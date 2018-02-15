c5aefe0cd0d8d2be5456f481294ba016e49d6d31
Jinfei Fan <fanjinfei@hotmail.com>
2018-02-15 13:12:39 -0500

    add timestamp range facet query

--- demo_flask.py
+++ demo_flask.py
@@ -75,6 +75,39 @@ def _get_search(q, start_page=1, page_size=20, labels=None, burl=None, lang='en'
     else:
         return None
 
+date_range = {
+    "timestamp:[now-1d/d TO *]":[0,'past day'], 
+    "timestamp:[now-7d/d TO *]":[1,'past week'],
+    "timestamp:[now-30d/d TO *]":[2, 'past month'],
+    "timestamp:[now-1y/d TO *]":[3, 'past year'],
+    "timestamp:[* TO now-1y-1d/d]":[4, 'one year older'],
+}
+def _get_search_post(q, start_page=1, page_size=20, labels=None, burl=None, lang='en', sort='', ex_q=''):
+    start = (start_page-1)*page_size
+    if not burl: burl=base_url
+    if burl[-1] == '?': burl = burl[:-1]
+
+    payload= {'q':q,
+              'start':start,
+              'num':page_size,
+              'lang':lang,
+              'sort':sort,
+              'facet.query':list(date_range.keys())
+            }
+    if labels:
+        payload['fields.label'] = labels
+    if ex_q:
+        payload['ex_q'] = ex_q
+
+    print (burl, payload)
+    user_agent = {'User-agent': 'statcan search'}
+    r = requests.post(url=burl, headers=user_agent, data=payload, timeout=10)
+    if r.status_code == requests.codes.ok:
+        print r.text[-200:]
+        return r.text
+    else:
+        return None
+
 @app.route("/suggest", methods=['GET'])
 def suggest():
     qval = request.args.get('query')
@@ -157,11 +190,13 @@ def qfilter(val):
 def ecn_search():
     qval = request.args.get('q')
     sort = request.args.get('sort', '')
+    ex_q = request.args.get('ex_q', '')
     print type(qval), qval
     page = request.args.get('page', 1, type=int)
     print (qval, request.get_json(), request.data, request.args)
     res = None
     pagination = None
+    facet = {}
     
     sub = request.args.get('sub', '')
     if not sub or sub=='all':
@@ -184,14 +219,32 @@ def ecn_search():
     urls['phone'] = url_sbase + '&sub=phone'
     urls['statcan'] = url_sbase + '&sub=statcan'
     lang = get_locale()
+    def get_facet_query(res):
+        if not res: return []
+        data = []
+        for item in res:
+            val, count = item['value'], item['count']
+            fa_label = date_range.get(val, 'unknown')
+            data.append( [fa_label[0], fa_label[1], count, val])
+        data.sort(key=lambda x:x[0])
+        facet_burl = u'/{0}/ecn_search?q={1}&sort={2}&sub={3}&ex_q='.format(
+                    get_locale(),qval or '', sort, sub)
+        ndata = []
+        for x in data:
+            if x[3]== ex_q:
+                ndata.append( [x[1], x[2], facet_burl, 1])
+            else:
+                ndata.append( [x[1], x[2], facet_burl+x[3], 0])
+        return ndata
     if qval:
         #TODO: escape : -> \:
 #        res = _get_search(qval.replace(' ', '+'), page, 20, labels,
 #                          'http://f7wcmstestb2.statcan.ca:9601/json/?', lang)
-        res = _get_search(qfilter(qval), page, 20, labels,
-                          'http://f7wcmstestb2.statcan.ca:9601/json/?', lang, sort)
+        res = _get_search_post(qfilter(qval), page, 20, labels,
+                          'http://f7wcmstestb2.statcan.ca:9601/json/?', lang, sort, ex_q)
         if res:
             res = json.loads(res)['response']
+            facet = get_facet_query(res.get('facet_query', None))
             total, per_page = res.get('record_count', 0), 20
             total = min(total, 10000) # elastic search index.max_result_window [default] = 10000
             print 'total', total
@@ -207,7 +260,7 @@ def ecn_search():
                                     href = href, bs_version=4,
                                     total=total, record_name='users')
     return render_template('index_ecn.html', qval=qval or '', sub=sub, urls=urls, res=res, locale=get_locale(),
-                           pagination=pagination, sort=sort)
+                           pagination=pagination, sort=sort, facet=facet, ex_q=ex_q)
 
 
 @app.route("/<lang_code>/adv_search", methods=['GET'])
